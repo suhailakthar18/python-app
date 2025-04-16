bf98b9baadc52afab27491169adeadcc854b8565
suhailakthar18 <130057751+suhailakthar18@users.noreply.github.com>
2025-04-16 21:20:52 +0530

    Update port

--- app.py
+++ app.py
@@ -102,5 +102,5 @@ def reset_password():
     return render_template("reset_password.html", message=message)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
